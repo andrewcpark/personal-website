8d1a723696e0cb4e69521d2f4ddb8445892a8f90
Andrew Park <andrewchanwonpark@gmail.com>
2022-09-29 14:39:00 -0700

    updated navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e50dc61e55a8078"; }
+/******/ 		__webpack_require__.h = function() { return "9f89b94e20fd8ee8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
