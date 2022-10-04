b1a8db0b082a307d4d50ac4de4b96743114b66af
Andrew Park <andrewchanwonpark@gmail.com>
2022-10-04 11:26:37 -0700

    udpated png path

--- pages/nftGenerator.jsx
+++ pages/nftGenerator.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import nftGeneratorImg from '/public/assets/projects/nftgenerator.png'
+import nftGeneratorImg from '/public/assets/projects/nftGenerator.png'
 import Image from 'next/image'
 import {RiRadioButtonFill} from 'react-icons/ri'
 import Link from 'next/link'
