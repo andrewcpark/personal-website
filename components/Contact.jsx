b45edd234517b28f9d592bc79606216f03a2091f
Andrew Park <andrewchanwonpark@gmail.com>
2022-11-01 09:34:48 -0700

    updated captcha

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -87,7 +87,7 @@ const Contact = () => {
                             <label className='uppercase text-sm py-2'> Message </label>
                             <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10' name='message'/>
                         </div>
-                        <input type="hidden" id="captchaResponse" name="g-recaptcha-response"></input>
+                        <div class="g-recaptcha" data-sitekey="6Lcd7s4iAAAAAO8CYDaoDJk9m6moyzYt6CKOes4K"></div>
                         <button className='w-full p-4 text-gray-100 mt-4'> Send Message </button>
                     </form>
                 </div>
