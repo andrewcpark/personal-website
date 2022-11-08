778524a7c3ddcaaae5cd6f5157db85c4da1d0a14
Andrew Park <andrewchanwonpark@gmail.com>
2022-11-08 12:12:12 -0800

    updated post layout

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -15,7 +15,9 @@ const PostPage = ({frontmatter: {title, date, cover_image}, slug, content}) => {
         <div className='h-200 text-center'>
             <h1 className='mt-5'>{title}</h1>
             <p className='p-8'>{date}</p>
+            <br/>
             <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+            <br/>
             <Link href='/#blog'><button className='px-6 py-2 mt-4'> Go Back </button></Link>
         </div>
     </div>
--- posts/firstYear.md
+++ posts/firstYear.md
@@ -5,4 +5,6 @@ excerpt: "This is the excerpt for the post"
 cover_image: "/assets/images/firstYear.jpg"
 ---
 
-More to come.
+<img src="https://images.unsplash.com/photo-1644792863360-40fa85ea52e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="Hello World Photo" style="width:50%; display: block; margin-left: auto; margin-right: auto;"/>
+<br>
+<p> More to come. </p>
